7ca8f6366f38c24ba4332810d94cb89d00efb93d
Simon Dirmeier <simd23@protonmail.com>
2026-06-27 00:50:51 +0200

    build: editable C++ auto-rebuild, build deps, version 0.2.0

--- src/sabc/__init__.py
+++ src/sabc/__init__.py
@@ -1,11 +1,10 @@
-"""sabc - Simulated Annealing ABC in MLX C++."""
+"""sabc - Simulated Annealing ABC in MLX."""
 
 from sabc._src import distributions
 from sabc._src.proposals import DiffEvolution
 from sabc._src.run import Posterior, run
 from sabc._src.schedule import MultiEps, SingleEps
 
-__version__ = "0.1.0"
 __all__ = [
   "DiffEvolution",
   "MultiEps",
